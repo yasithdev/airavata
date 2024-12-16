261652896c21e7d48e593ecc980fce5729374112
yasith <yasith.j@icloud.com>
2024-12-15 21:06:25 -0600

    remove runtime configs from settings.ini, ability to pick runtimes by cluster/category. change base directory for agent

--- modules/agent-framework/airavata-agent/build-container.sh
+++ modules/agent-framework/airavata-agent/build-container.sh
@@ -1,4 +1,4 @@
 env GOOS=linux GOARCH=amd64 go build
 cp airavata-agent airavata-agent-linux
-docker build --platform linux/x86_64 -t dimuthuupe/airavata-agent .
-docker push dimuthuupe/airavata-agent 
+docker build --platform linux/x86_64 -t yasithmilinda/airavata-agent .
+docker push yasithmilinda/airavata-agent 
